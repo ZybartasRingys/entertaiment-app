9853613b6d61657651dd0d9fc622eee4a2ac7957
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-01-17 18:15:35 +0200

    application start

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+const port = 5000;
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/movies");
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
