70228b237c94c028d687861a3494569e5016a3f6
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-01-23 09:47:52 +0200

    bookmarked cards

--- frontend/src/components/BookmarkedCard.jsx
+++ frontend/src/components/BookmarkedCard.jsx
@@ -1,6 +1,74 @@
+/* Styled Componenets imports*/
+import {
+  CardTop,
+  CardBottom,
+  CardDiv,
+  RecomendedCard,
+} from "./styles/Recomended.styled";
+
+/* Styled Componenets imports*/
+
+import {
+  IconDiv,
+  TitleDiv,
+  Div,
+  IconContainer,
+} from "./styles/Trending.styled";
+
+// Context
+import { useStateContext } from "../Context/StateContext";
+
+// icons
+
+import { ReactComponent as BookEmpty } from "../assets/icon-bookmark-empty.svg";
+import { ReactComponent as BookFull } from "../assets/icon-bookmark-full.svg";
+import { ReactComponent as TvIcon } from "../assets/icon-category-tv.svg";
+import { ReactComponent as MovieIcon } from "../assets/icon-category-movie.svg";
+import { BsDot } from "react-icons/bs";
+
 function BookmarkedCard({ movie }) {
   const { title } = movie;
-  return <div>{title}</div>;
+  const { isBookmarked, setIsBookmarked } = useStateContext();
+  return (
+    <div>
+      <RecomendedCard key={movie._id}>
+        <CardTop>
+          <IconContainer>
+            {isBookmarked ? (
+              <IconDiv type="button" onClick={setIsBookmarked(true)}>
+                <BookEmpty />
+              </IconDiv>
+            ) : (
+              <IconDiv type="button" onClick={setIsBookmarked(false)}>
+                <BookFull />
+              </IconDiv>
+            )}
+          </IconContainer>
+        </CardTop>
+
+        <CardBottom>
+          <CardDiv>
+            <Div>
+              <p>{movie.year}</p>
+              <BsDot size={12} />
+            </Div>
+            <Div>
+              {movie.category === "Movie" ? <MovieIcon /> : <TvIcon />}
+              <p>{movie.category}</p>
+            </Div>
+            <Div>
+              <BsDot size={11} />
+              <p>{movie.rating}</p>
+            </Div>
+          </CardDiv>
+
+          <TitleDiv>
+            <h2>{movie.title}</h2>
+          </TitleDiv>
+        </CardBottom>
+      </RecomendedCard>
+    </div>
+  );
 }
 
 export default BookmarkedCard;
