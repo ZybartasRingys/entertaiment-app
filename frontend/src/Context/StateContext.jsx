bb19063f7a35256a23002c5102e92cceaacba14f
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-03-20 10:16:12 +0200

    bookmarked useEffect

--- frontend/src/Context/StateContext.jsx
+++ frontend/src/Context/StateContext.jsx
@@ -1,45 +1,43 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import axios from "axios";
+import { createContext, useContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
-import { useAuthContext } from "../hooks/useAuthContext";
+import { useAuthContext } from '../hooks/useAuthContext'
 
-const Context = createContext();
+const Context = createContext()
 
 export const StateContext = ({ children }) => {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [trending, setTrending] = useState([]);
-  const [bookmarked, setBookmarked] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
-  const { user } = useAuthContext();
-  const [recommended, setRecommended] = useState([]);
-
-  console.log(bookmarked);
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [trending, setTrending] = useState([])
+  const [bookmarked, setBookmarked] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const { user } = useAuthContext()
+  const [recommended, setRecommended] = useState([])
 
   /* useEffects */
 
   useEffect(() => {
-    getBookmarkedMovies();
-  }, []);
+    getBookmarkedMovies()
+  }, [user])
 
   useEffect(() => {
-    setTrendingMovies();
-  }, [movies]);
+    setTrendingMovies()
+  }, [movies])
 
   useEffect(() => {
-    getMovies();
-  }, [bookmarked]);
+    getMovies()
+  }, [bookmarked])
 
   useEffect(() => {
-    setRecommendedMovies();
-  }, [movies]);
+    setRecommendedMovies()
+  }, [movies])
 
   useEffect(() => {
-    const bookmarkedItems = JSON.parse(localStorage.getItem("bookmarkedMovie"));
+    const bookmarkedItems = JSON.parse(localStorage.getItem('bookmarkedMovie'))
     if (bookmarkedItems) {
-      setBookmarked([...bookmarked, bookmarkedItems]);
+      setBookmarked([...bookmarked, bookmarkedItems])
     }
-  }, []);
+  }, [])
 
   /**
    * "getMovies" is an async function that sets the loading state to true, then it makes an axios request
@@ -49,17 +47,16 @@ export const StateContext = ({ children }) => {
 
   const getMovies = async () => {
     try {
-      setLoading(true);
-      const response = await axios.get("http://localhost:5000/movies");
+      setLoading(true)
+      const response = await axios.get('http://localhost:5000/movies')
 
-      setMovies(response.data);
-      setSearchResults(response.data);
-
-      setLoading(false);
+      setMovies(response.data)
+      setSearchResults(response.data)
+      setLoading(false)
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
-  };
+  }
 
   /**
    * The setTrendingMovies function filters through the movies array and returns the movies that have
@@ -69,18 +66,18 @@ export const StateContext = ({ children }) => {
   const setTrendingMovies = () => {
     const trendingMovies = searchResults.filter(
       (trendingMovie) => trendingMovie.isTrending === true
-    );
+    )
 
-    setTrending(trendingMovies);
-  };
+    setTrending(trendingMovies)
+  }
 
   const setRecommendedMovies = () => {
     const recommendedMovies = movies.filter(
       (movie) => movie.isTrending === false
-    );
+    )
 
-    setRecommended(recommendedMovies);
-  };
+    setRecommended(recommendedMovies)
+  }
 
   /**
    * GetBookmarkedMovies() is an async function that uses axios to make a GET request to the server, and
@@ -89,21 +86,21 @@ export const StateContext = ({ children }) => {
 
   const getBookmarkedMovies = async () => {
     try {
-      setLoading(true);
+      setLoading(true)
       const response = await axios({
-        method: "GET",
+        method: 'GET',
         url: `http://localhost:5000/movies/bookmarked`,
         headers: {
           Authorization: `Bearer ${user.token}`,
         },
-      });
+      })
 
-      setBookmarked(response.data);
-      setLoading(false);
+      setBookmarked(response.data)
+      setLoading(false)
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
-  };
+  }
 
   /**
    * It takes the _id of the movie that the user wants to bookmark, and then it sends a PUT request to
@@ -118,11 +115,11 @@ export const StateContext = ({ children }) => {
    * The bookmarked array is then used to render the bookmarked movies.
    */
   const addBookmark = async (_id) => {
-    console.log(_id);
+    console.log(_id)
     try {
-      setLoading(true);
+      setLoading(true)
       const response = await axios({
-        method: "PUT",
+        method: 'PUT',
         url: `http://localhost:5000/movies/${_id}`,
         headers: {
           Authorization: `Bearer ${user.token}`,
@@ -130,15 +127,14 @@ export const StateContext = ({ children }) => {
         data: {
           isBookmarked: true,
         },
-      });
-      setBookmarked([...bookmarked, response.data]);
-      setRecommended([...recommended]);
-      localStorage.setItem("bookmarkedMovie", JSON.stringify([response.data]));
-      setLoading(false);
+      })
+      setBookmarked([...bookmarked, response.data])
+      setRecommended([...recommended])
+      setLoading(false)
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
-  };
+  }
 
   /**
    * It takes the _id of a movie and sends a PUT request to the server to update the movie's
@@ -147,7 +143,7 @@ export const StateContext = ({ children }) => {
   const remBookmark = async (_id) => {
     try {
       const response = await axios({
-        method: "PUT",
+        method: 'PUT',
         url: `http://localhost:5000/movies/${_id}`,
         headers: {
           Authorization: `Bearer ${user.token}`,
@@ -155,15 +151,14 @@ export const StateContext = ({ children }) => {
         data: {
           isBookmarked: false,
         },
-      });
+      })
       setBookmarked(
         bookmarked.filter((movie) => movie._id !== response.data._id)
-      );
-      console.log(response.data);
+      )
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
-  };
+  }
 
   return (
     <Context.Provider
@@ -184,7 +179,7 @@ export const StateContext = ({ children }) => {
     >
       {children}
     </Context.Provider>
-  );
-};
+  )
+}
 
-export const useStateContext = () => useContext(Context);
+export const useStateContext = () => useContext(Context)
